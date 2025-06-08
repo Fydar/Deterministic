16a4d218f93f2e69428643971bf953c79576dcdc
fydar <fydar.dev@gmail.com>
2025-06-08 20:06:36 +0100

    🚧 Added shorthands for zero, min, and max

--- src/Fydar.Deterministic.Numerics/Fixed.cs
+++ src/Fydar.Deterministic.Numerics/Fixed.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Fydar.Deterministic.Numerics;
 
 /// <summary>
@@ -8,4 +10,34 @@ namespace Fydar.Deterministic.Numerics;
 /// </remarks>
 public readonly struct Fixed
 {
+    /// <summary>
+    /// <para>Represents a zero value.</para>
+    /// </summary>
+    /// <value><c>0</c></value>
+    /// <seealso cref="MinValue"/>
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    public static Fixed Zero { get; } = new(0L);
+
+    /// <summary>
+    /// <para>Represents the smallest possible value of <see cref="Fixed"/>.</para>
+    /// </summary>
+    /// <value><c>-140,737,488,355,328.00000</c></value>
+    /// <seealso cref="MaxValue"/>
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    public static Fixed MinValue { get; } = new(long.MinValue);
+
+    /// <summary>
+    /// <para>Represents the largest possible value of <see cref="Fixed"/>.</para>
+    /// </summary>
+    /// <value><c>140,737,488,355,328.00000</c></value>
+    /// <seealso cref="MinValue"/>
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    public static Fixed MaxValue { get; } = new(long.MaxValue);
+
+    private readonly long rawValue;
+
+    internal Fixed(in long rawValue)
+    {
+        this.rawValue = rawValue;
+    }
 }
