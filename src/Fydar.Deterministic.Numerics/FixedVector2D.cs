16fd9afa64855daef031f69c3c004724b824faca
fydar <fydar.dev@gmail.com>
2025-06-08 19:37:36 +0100

    🚧 Adding signatures for 2D and 3D fixed vector types

---
+++ src/Fydar.Deterministic.Numerics/FixedVector2D.cs
@@ -0,0 +1,9 @@
+namespace Fydar.Deterministic.Numerics;
+
+/// <summary>
+/// Represents a vector with two fixed-point values.
+/// </summary>
+/// <seealso cref="FixedVector3D"/>
+public readonly struct FixedVector2D
+{
+}
---
+++ src/Fydar.Deterministic.Numerics/FixedVector3D.cs
@@ -0,0 +1,9 @@
+namespace Fydar.Deterministic.Numerics;
+
+/// <summary>
+/// Represents a vector with three fixed-point values.
+/// </summary>
+/// <seealso cref="FixedVector2D"/>
+public readonly struct FixedVector3D
+{
+}
