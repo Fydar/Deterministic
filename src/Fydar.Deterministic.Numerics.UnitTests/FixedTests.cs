6f83b6d69060afea8c4506614f9dc2c84b439000
fydar <fydar.dev@gmail.com>
2025-06-08 22:32:06 +0100

    🚧 Added tests for shorthand Fixed numeric values

---
+++ src/Fydar.Deterministic.Numerics.UnitTests/FixedTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Fydar.Deterministic.Numerics.UnitTests;
+
+/// <summary>
+/// Unit tests for the <see cref="Fixed"/> type.
+/// </summary>
+public class FixedTests
+{
+    [Fact]
+    public void ApproximatePi()
+    {
+        // Arrange
+        var pi = Fixed.Pi;
+
+        // Act
+        string piString = pi.ToString();
+
+        // Assert
+        Assert.Equal("3.14159", piString);
+    }
+
+    [Fact]
+    public void ApproximateTau()
+    {
+        // Arrange
+        var tau = Fixed.Tau;
+
+        // Act
+        string tauString = tau.ToString();
+
+        // Assert
+        Assert.Equal("6.28317", tauString);
+    }
+}
