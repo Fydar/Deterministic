e81df1cf23555332565e533536cb983780e0e1e6
fydar <fydar.dev@gmail.com>
2025-06-08 19:02:06 +0100

    🚧 Creating project structure and basic numeric type

---
+++ src/Fydar.Deterministic.Numerics/Fixed.cs
@@ -0,0 +1,11 @@
+namespace Fydar.Deterministic.Numerics;
+
+/// <summary>
+/// <para>Represents a deterministic fixed-point number.</para>
+/// </summary>
+/// <remarks>
+/// <para>The <see cref="Fixed"/> value type represents a 64-bit number with values ranging <b>from</b> <c>-140,737,488,355,328.00000</c> <b>to</b> <c>-140,737,488,355,328.00000</c>.</para>
+/// </remarks>
+public readonly struct Fixed
+{
+}
