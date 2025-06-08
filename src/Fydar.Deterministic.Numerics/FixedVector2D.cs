3f15c7c8226a89a29cce2aca7893833c58db3b9a
fydar <fydar.dev@gmail.com>
2025-06-08 20:47:10 +0100

    🚧 Added the components to the vector types

--- src/Fydar.Deterministic.Numerics/FixedVector2D.cs
+++ src/Fydar.Deterministic.Numerics/FixedVector2D.cs
@@ -6,4 +6,13 @@ namespace Fydar.Deterministic.Numerics;
 /// <seealso cref="FixedVector3D"/>
 public readonly struct FixedVector2D
 {
+    /// <summary>
+    /// <para>The X component of the vector.</para>
+    /// </summary>
+    public readonly Fixed X { get; }
+
+    /// <summary>
+    /// <para>The Y component of the vector.</para>
+    /// </summary>
+    public readonly Fixed Y { get; }
 }
--- src/Fydar.Deterministic.Numerics/FixedVector3D.cs
+++ src/Fydar.Deterministic.Numerics/FixedVector3D.cs
@@ -6,4 +6,18 @@ namespace Fydar.Deterministic.Numerics;
 /// <seealso cref="FixedVector2D"/>
 public readonly struct FixedVector3D
 {
+    /// <summary>
+    /// <para>The X component of the vector.</para>
+    /// </summary>
+    public readonly Fixed X { get; }
+
+    /// <summary>
+    /// <para>The Y component of the vector.</para>
+    /// </summary>
+    public readonly Fixed Y { get; }
+
+    /// <summary>
+    /// <para>The Z component of the vector.</para>
+    /// </summary>
+    public readonly Fixed Z { get; }
 }
